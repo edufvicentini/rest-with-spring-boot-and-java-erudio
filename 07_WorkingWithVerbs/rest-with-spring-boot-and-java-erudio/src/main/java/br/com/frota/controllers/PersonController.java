b9930528d795a2fd83c49d2e96655a2b06447ad5
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2023-07-01 14:52:56 -0300

    basic crud

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/frota/controllers/PersonController.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/frota/controllers/PersonController.java
@@ -1,27 +1,66 @@
 package br.com.frota.controllers;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.frota.model.Person;
 import br.com.frota.services.PersonServices;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
 	private PersonServices service;
 	
-	@RequestMapping(value = "/{operator}/{pathNumbers}", method = RequestMethod.GET)
-	public Double calculate(
-			@PathVariable(value = "operator") String operator,
-			@PathVariable(value = "pathNumbers") String pathNumbers
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(
+			@PathVariable(value = "id") String id
 		) throws Exception {		
-
-
-		return 0D;
+		
+		return service.findById(id);
+	}
 	
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() throws Exception {		
+		return service.findAll();
+	}
+	
+	@RequestMapping(
+			method = RequestMethod.POST, 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(
+			@RequestBody Person person
+			) throws Exception {		
+		return service.create(person);
+	}
+	
+	@RequestMapping(
+			method = RequestMethod.PUT, 
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(
+			@RequestBody Person person
+			) throws Exception {		
+		return service.update(person);
+	}
+	
+	@RequestMapping(value = "/{id}",
+			method = RequestMethod.DELETE
+			)
+	public void update(
+			@PathVariable(value = "id") String id
+			) throws Exception {		
+		service.delete(id);
 	}
 }
