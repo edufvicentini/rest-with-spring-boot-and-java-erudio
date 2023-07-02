f6a708257036423546f09fcdd2f23ef553309aaf
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2023-07-01 23:27:53 -0300

    database working

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/frota/controllers/PersonController.java
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/frota/controllers/PersonController.java
@@ -24,7 +24,7 @@ public class PersonController {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(
-			@PathVariable(value = "id") String id
+			@PathVariable(value = "id") Long id
 		) throws Exception {		
 		
 		return service.findById(id);
@@ -59,7 +59,7 @@ public class PersonController {
 			method = RequestMethod.DELETE
 			)
 	public void update(
-			@PathVariable(value = "id") String id
+			@PathVariable(value = "id") Long id
 			) throws Exception {		
 		service.delete(id);
 	}
