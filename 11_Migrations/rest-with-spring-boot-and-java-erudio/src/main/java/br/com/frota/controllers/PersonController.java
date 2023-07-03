c264436aa9ae0855649c88a48e5dded418b85435
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2023-07-03 00:56:03 -0300

    migrations

--- 11_Migrations/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/frota/controllers/PersonController.java
+++ 11_Migrations/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/frota/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.com.frota.data.vo.v1.PersonVO;
 import br.com.frota.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("api/person/v1")
 public class PersonController {
 	
 	@Autowired
---
+++ 11_Migrations/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (5,'Tubarão - Santa Catarina - Brasil','Eduardo','Male','Frota');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (6,'Tubarão - Salplplplpnta Catarina - Brasil asdadwawd','Eduardo','Male','Frota');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (7,'Tubarão - Santa Catarina - Brasil','Eduardo','Male','Frota');
